68176ad7958c3657b7df9dc28aebb838b0c8783d
moritzknie3-cpu <moritzknie3@gmail.com>
2026-03-04 10:41:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-// Loader
 window.addEventListener('load',()=>{
-setTimeout(()=>{
-document.getElementById('loader').style.display='none';
-},1500);
+  setTimeout(()=>{
+    document.getElementById('loader').style.display='none';
+  },1000);
 
-document.querySelectorAll('.bar').forEach(bar=>{
-bar.style.width=bar.getAttribute('data-progress');
+  document.querySelectorAll('.bar').forEach(bar=>{
+    bar.style.width=bar.getAttribute('data-progress');
+  });
 });
-});
-
-// Sound
-function playIntro(){
-document.getElementById('introSound').play();
-}
-
-// Lightbox
-function openLightbox(src){
-document.getElementById('lightbox').style.display='flex';
-document.getElementById('lightbox-img').src=src;
-}
-function closeLightbox(){
-document.getElementById('lightbox').style.display='none';
-}
